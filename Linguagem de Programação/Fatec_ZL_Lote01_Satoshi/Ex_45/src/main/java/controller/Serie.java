2e142c7c387a5ebb42e384688c03b4aaef03f878
Rafael-GSousa <rafael.sousa37@fatec.sp.gov.br>
2022-10-01 14:00:42 -0300

    update Ex_45

--- Linguagem de Programação/Fatec_ZL_Lote01_Satoshi/Ex_45/src/main/java/controller/Serie.java
+++ Linguagem de Programação/Fatec_ZL_Lote01_Satoshi/Ex_45/src/main/java/controller/Serie.java
@@ -1,5 +1,6 @@
 package controller;
 
+
 public class Serie {
     public Serie(){
         super();
@@ -7,7 +8,7 @@ public class Serie {
 
     public void sr(){
         int dividendoA = 1, divisorA = 1, dividendoB = 1, divisorB = 1;
-        double sr;
+        double sr = 1;
         System.out.print("Série ==> ");
         for (int i = 1; i <= 15; i++) {
             System.out.print(dividendoA + "/" + divisorA + " ");
@@ -15,13 +16,17 @@ public class Serie {
             divisorA = (int) Math.pow(dividendoA, 2);
         }
 
-//        System.out.print("\nResultado da série ==> ");
-//        for (int j = 1; j <= 15; j++) {
-//            sr = (double)dividendoB / divisorB;
-//            System.out.print(sr + " ");
-//            dividendoB++;
-//            divisorB+=2;
-//        }
+        System.out.print("\nResultado da série ==> ");
+        while (dividendoB < 15) {
+            if(dividendoB % 2 == 0){
+                dividendoB = (-(dividendoB)) + 1;
+            }else{
+                dividendoB = (-(dividendoB)) - 1;
+            }
+            divisorB = (int) Math.pow(dividendoB, 2);
+            sr = sr + ((double)dividendoB / divisorB);
+            System.out.print(sr + " ");
+        }
 
     }
 }
